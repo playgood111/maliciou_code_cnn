8c13ccc4d5cf28a18dddb2f9eafe46f05650c4e7
playgood111 <playgood333@gmail.com>
2016-09-29 03:49:34 -0500

    Add files via upload

--- get_train_data.py
+++ get_train_data.py
@@ -23,21 +23,22 @@ def get_dataset(name, split_name, dataset_dir):
           [], tf.int64, default_value=tf.zeros([], dtype=tf.int64)),
     }
 
-    items_to_handlers = {
-      'image': slim.tfexample_decoder.Image(shape=[299, 299, 1], channels=1),
-      'label': slim.tfexample_decoder.Tensor('image/class/label', shape=[]),
+        items_to_handlers = {
+         'image': slim.tfexample_decoder.Image(shape=[299, 299, 1], channels=1),
+         'label': slim.tfexample_decoder.Tensor('image/class/label', shape=[]),
     }
 
-    decoder = slim.tfexample_decoder.TFExampleDecoder(
-      keys_to_features, items_to_handlers)
+        decoder = slim.tfexample_decoder.TFExampleDecoder(
+         keys_to_features, items_to_handlers)
 
-    labels_to_names = None
-    dataset_dir = dataset_dir+'/'+name+'.tfrecords'
-    return slim.dataset.Dataset(
-        data_sources=dataset_dir,
-        reader=reader,
-        decoder=decoder,
-        num_samples=_SPLITS_TO_SIZES[split_name],
-        num_classes=_NUM_CLASSES,
-        items_to_descriptions=_ITEMS_TO_DESCRIPTIONS,
-        labels_to_names=labels_to_names)
+
+        labels_to_names = None
+        dataset_dir = dataset_dir+'/'+name+'.tfrecords'
+        return slim.dataset.Dataset(
+             data_sources=dataset_dir,
+             reader=reader,
+             decoder=decoder,
+             num_samples=_SPLITS_TO_SIZES[split_name],
+             num_classes=_NUM_CLASSES,
+             items_to_descriptions=_ITEMS_TO_DESCRIPTIONS,
+             labels_to_names=labels_to_names)
